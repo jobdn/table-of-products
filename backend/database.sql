8d99e56ec2b7734895461f31ff57022051194034
Danila <peace_are_off@bk.ru>
2022-07-15 07:32:54 +0300

    feat: add more description

--- backend/database.sql
+++ backend/database.sql
@@ -1,7 +1,11 @@
+-- run this command to create database with `new_db` name
+create database new_db;
+
+-- run this command to create table on database 
 create TABLE product(
     id SERIAL PRIMARY KEY,
     date DATE,
     name VARCHAR(255),
     amount INT,
-    distance INT,
+    distance INT
 );
--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,9 @@ const Pool = require("pg").Pool;
 
 // Adjust this object
 const pool = new Pool({
+  // Put here your user in postgres
   user: "postgres",
-  password: "postgres",
+  password: "123",
   host: "localhost",
   port: 5432,
   database: "new_db",
